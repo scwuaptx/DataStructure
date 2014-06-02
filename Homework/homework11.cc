7b7a97a97998b0f5448fb0a7dcf73d30d7b96edb
Angelboy <scwuaptx@gmail.com>
2014-06-03 01:15:53 +0800

    modify homework11

--- Homework/homework11.cc
+++ Homework/homework11.cc
@@ -47,11 +47,14 @@ class Priority_Queue{
 	
 	vector<T> data ;
 
+	//Node of son
 	int leftson( int i ) const { return 2 * i + 1 ; }
 	int rightson( int i ) const { return 2 * i + 2 ; }
 
+	//Node of Parent
 	int parent( int i ) const { return (i-1)/2 ; }
 
+	//whether is a node
 	bool tree_node( int i ) const {
 		return ( i < data.size() ? true:false ) ;
 	}
@@ -60,7 +63,7 @@ class Priority_Queue{
 		return static_cast<int>(log(i+1)/log(2)+SMALL) ;
 	}
 
-
+	//reheapification upward
 	void reheap_upward( int i = 0 ){
 		int p = parent(i) ;
 
@@ -73,6 +76,7 @@ class Priority_Queue{
 	}
 	public :
 
+		//constructor
 		Priority_Queue() {}
 		
 		Priority_Queue( const T* a , const T* b){
@@ -83,16 +87,19 @@ class Priority_Queue{
     		for ( int i = 0 ; i < f.size() ; i++ ) push(f[i]) ;
 		 }
 
-
+		//Number of data
 		int size() const { return data.size() ; }
 
+		//push a element
 		void push( const T& a ){
 			data.push_back(a) ;
 			reheap_upward( size() - 1 ) ;
 		}
 
+		//element of top
 		T top() const { return data[0] ; }
 
+		//print the tree
 		void print_queue_as_tree( int i , ostream& out ) const {
 			if(tree_node(i)){
 				print_queue_as_tree(rightson(i),out) ;
@@ -102,6 +109,7 @@ class Priority_Queue{
 			}
 		}
 
+		//output operator
 		friend ostream& operator<< (ostream& out , const Priority_Queue& foo ){
 			foo.print_queue_as_tree(0,out) ;
 			return out << endl ;
@@ -112,6 +120,7 @@ class Priority_Queue{
 int main() {
 	const int N = 9 , M = 6 ;
 
+	//read the file
 	ifstream infile("score.txt") ;
 	string line ,name ;
 	int old ;
@@ -125,6 +134,7 @@ int main() {
 		stu.name = name ;
 		istring >> old ;
 		stu.old = old ;
+		//push a node
 		Studentlist.push_back(stu) ;
 		istring.clear() ;
 	}
